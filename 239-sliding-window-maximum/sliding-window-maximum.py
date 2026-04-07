f148f85e1c1d4242d3996b4d555149c7cf63704a
Bethe Bayou Belete <bethebayou@gmail.com>
2026-04-07 19:09:09 +0300

    Time: 185 ms (64.79%) | Memory: 34.6 MB (92.37%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        dq = deque()
+        res = []
+        for i, val in enumerate(nums):
+            while dq and nums[dq[-1]] <= val:
+                dq.pop()
+
+            dq.append(i)
+            if dq[0] <= i - k:
+                dq.popleft()
+            if i >= k - 1:
+                res.append(nums[dq[0]])
+
+        return res
